328805ec4bcc64e2440453a25183cbacf5a670e8
YangWen <yangwen99@163.com>
2013-05-31 09:45:17 +0800

    修改测试

--- bbbtest/szbs_chkdis_name.php
+++ bbbtest/szbs_chkdis_name.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 if($key=='field2'){
 	if(DB::result_first("SELECT count(*) FROM ".DB::table('ucenter_members')." where username='".$value."'")>0||DB::result_first("SELECT count(*) FROM ".DB::table('common_member_profile')." where uid<>".$_G['uid']." and field2='".$value."'")>0||chk_wp_username($value)>0){
 		profile_showerror('field2');
 	}
 }
-
+//我在这里修改一下
 //dump('dawsdsa');
 
 
